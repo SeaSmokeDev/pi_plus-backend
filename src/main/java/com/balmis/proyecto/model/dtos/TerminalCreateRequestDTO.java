7c05aa8e5ffade6292ae1580f1ea08b24e0f4def
Ian tauzy <ian.dev@outlook.es>
2026-05-30 20:23:13 +0200

    dtos para crear terminal

---
+++ src/main/java/com/balmis/proyecto/model/dtos/TerminalCreateRequestDTO.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.balmis.proyecto.model.dtos;
+
+import com.balmis.proyecto.model.EstadoTerminal;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Ian
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TerminalCreateRequestDTO {
+
+    @NotBlank(message = "La marca es obligatoria")
+    @Size(max = 100, message = "La marca no puede tener más de 100 caracteres")
+    private String marca;
+
+    @NotBlank(message = "El modelo es obligatorio")
+    @Size(max = 100, message = "El modelo no puede tener más de 100 caracteres")
+    private String modelo;
+
+    @NotNull(message = "El estado es obligatorio")
+    private EstadoTerminal estado;
+
+    @Size(max = 255, message = "Las notas no pueden tener más de 255 caracteres")
+    private String notas;
+}
